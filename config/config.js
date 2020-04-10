6c5affbed6f2d5715b34a520fe4c8b89db8bbf33
cpanzenbock <cpanzenbock@gmail.com>
2020-04-10 13:05:31 +1000

    Initialised Integration with DB
    Added sequelize and pg packages to connect with postgres database.

---
+++ config/config.js
@@ -0,0 +1,27 @@
+{
+  "development": {
+    "username": "fraudtech-dbuser",
+    "password": "Covid-19",
+    "database": "fraudtech-db",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "port" : 5432,
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
