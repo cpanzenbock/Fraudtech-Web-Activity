dab3f3499f0543ac9df161def70159ac8d5e26d7
cpanzenbock <cpanzenbock@gmail.com>
2020-04-15 08:57:47 +1000

    Updatable Records
    Updates to existing submissions are now working from the admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,14 @@
 const models = require('../models')
+const secret_url = "/971.911.39.241/";
+exports.secret_url = secret_url;
 
 exports.getInterface = function(req, res, next) {
-  models.request.findAll().then(enhancements => {
-    res.render('admin/admin', { title: 'Enhancement Requests', enhancements: enhancements });
+  models.request.findAll({
+    order: [
+      ['createdAt', 'DESC'],
+    ],
+  }).then(enhancements => {
+    res.render('admin/admin', { title: 'Enhancement Requests', enhancements: enhancements, url: secret_url });
   });
 }
 
@@ -10,26 +16,37 @@ exports.view = function(req, res, next) {
   models.request.findOne({
     where : {
       requestID : req.params.reqID
-    }
+    },
   }).then(enhancement => {
-    res.render('admin/record', { title: 'Enhancement Requests', e: enhancement });
+    res.render('admin/record', { title: 'Enhancement Requests', e: enhancement, url: secret_url });
   });
 }
 
 exports.showUpdate = function(req, res, next) {
+  let reqID = req.params.reqID;
   models.request.findOne({
     where : {
       requestID : req.params.reqID
-    }
+    },
   }).then(enhancement => {
-    res.render('landing', { title: 'Enhancement Requests', prefill: enhancement });
+    res.render('landing', { title: 'Enhancement Requests', prefill: enhancement, url: secret_url + reqID + '/update' });
   });
 }
 
 
 exports.update = function(req, res, next) {
-  // TODO have this actually change the record
-  return models.request.findAll().then(enhancements => {
-    res.render('admin/admin', { title: 'Enhancement Requests', enhancements: enhancements });
+  let reqID = req.params.reqID;
+
+  models.request.update({
+    user: req.body.logon,
+    feature: req.body.sel1,
+    description: req.body.describe,
+    usageNumbers: req.body.quantity,
+  }, {
+    where: {
+      requestID: reqID
+    }
+  }).then(result => {
+    res.redirect(secret_url + reqID);
   });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 let landing = require('../controllers/landing');
 let dashboard = require('../controllers/dashboard');
+let admin = require('../controllers/admin');
+
+
 
 /* GET home page. */
 router.get('/', landing.getLanding);
@@ -10,5 +13,10 @@ router.get('/', landing.getLanding);
 router.post('/', landing.submitEnhancement);
 /* GET dash page. */
 router.get('/status', dashboard.getDash);
+/* GET secret admin page. */
+router.get(admin.secret_url, admin.getInterface);
+router.get(admin.secret_url + ':reqID', admin.view);
+router.get(admin.secret_url + ':reqID/update', admin.showUpdate);
+router.post(admin.secret_url + ':reqID/update', admin.update);
 
 module.exports = router;
